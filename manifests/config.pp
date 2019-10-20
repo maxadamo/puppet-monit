73d227731aa54d9462ba5582d81ef89f715cd9e0
Massimiliano Adamo <maxadamo@gmail.com>
2019-10-20 16:22:31 +0200

    fix credentials

--- manifests/config.pp
+++ manifests/config.pp
@@ -26,10 +26,10 @@ class monit::config inherits monit {
   file { 'monit_config':
     ensure  => file,
     path    => $monit::config_file,
-    owner   => 'root',
-    group   => 'root',
+    owner   => root,
+    group   => root,
     mode    => '0600',
-    content => epp('monit/monitrc.epp', {
+    content => Sensitive(epp('monit/monitrc.epp', {
       check_interval            => $monit::check_interval,
       start_delay               => $monit::start_delay,
       logfile                   => $monit::logfile,
@@ -41,14 +41,14 @@ class monit::config inherits monit {
       httpd_address             => $monit::httpd_address,
       httpd_allow               => $monit::httpd_allow,
       httpd_user                => $monit::httpd_user,
-      httpd_password            => $monit::httpd_password_real,
-      mmonit_password           => $monit::mmonit_password_real,
+      httpd_password            => Sensitive($monit::httpd_password_real),
+      mmonit_password           => Sensitive($monit::mmonit_password_real),
       mmonit_address            => $monit::mmonit_address,
       mmonit_https              => $monit::mmonit_https,
       mmonit_without_credential => $monit::mmonit_without_credential,
       config_dir                => $monit::config_dir,
       monit_version_real        => $monit::monit_version_real
-    }),
+    })),
     require => Package['monit'],
   }
 
